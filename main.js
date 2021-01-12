255cb276e3fa6542c66633f2058725299f210f2d
Oksana Chorna <ksuchorna@gmail.com>
2021-01-12 22:11:22 +0200

    comit

--- main.js
+++ main.js
@@ -9,9 +9,10 @@ const getUserNames = users => {
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-// // Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 // const getUsersWithEyeColor = (users, color) => {
 //   // твой код
+//   return users.filter(user => user.eyeColor === color);
 // };
 
 // console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
