66e1f9d75abe4269e7b8daaa757c6eb0d570686d
Oksana Chorna <ksuchorna@gmail.com>
2021-01-12 22:39:25 +0200

    Зроблено "массив имен пользователей по полу"

--- main.js
+++ main.js
@@ -6,23 +6,24 @@ const getUserNames = users => {
   return users.map(user => user.name);
 };
 
-console.log(getUserNames(users));
+// console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
 // Получить массив объектов пользователей по цвету глаз (поле eyeColor).
-// const getUsersWithEyeColor = (users, color) => {
-//   // твой код
-//   return users.filter(user => user.eyeColor === color);
-// };
+const getUsersWithEyeColor = (users, color) => {
+  // твой код
+  return users.filter(user => user.eyeColor === color);
+};
 
 // console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
 // // Получить массив имен пользователей по полу (поле gender).
-// const getUsersWithGender = (users, gender) => {
-//   // твой код
-// };
+const getUsersWithGender = (users, gender) => {
+  // твой код
+  return users.filter(user => user.gender === gender).map(user => user.name);
+};
 
-// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 // Получить массив только неактивных пользователей(поле isActive).
 const getInactiveUsers = users => {
@@ -30,7 +31,7 @@ const getInactiveUsers = users => {
   return users.filter(user => !user.isActive);
 };
 
-console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
 // //Получить пользоваля (не массив) по email (поле email, он уникальный).
 // const getUserWithEmail = (users, email) => {
