36846576f501f1377c90a508d9a73e1f853af91d
Oksana Chorna <ksuchorna@gmail.com>
2021-01-18 19:48:11 +0200

    Розкоментувала завдання

--- main.js
+++ main.js
@@ -1,58 +1,58 @@
 import users from './users.js';
 
-// const getUserNames = users => {
-//   // твой код
-//   return users.map(user => user.name);
-// };
+const getUserNames = users => {
+  // твой код
+  return users.map(user => user.name);
+};
 
-// console.log(getUserNames(users));
-// // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-// const getUsersWithEyeColor = (users, color) => {
-//   // твой код
-//   return users.filter(user => user.eyeColor === color);
-// };
+const getUsersWithEyeColor = (users, color) => {
+  // твой код
+  return users.filter(user => user.eyeColor === color);
+};
 
-// console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-// const getUsersWithGender = (users, gender) => {
-//   // твой код
-//   return users.filter(user => user.gender === gender).map(user => user.name);
-// };
+const getUsersWithGender = (users, gender) => {
+  // твой код
+  return users.filter(user => user.gender === gender).map(user => user.name);
+};
 
-// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-// const getInactiveUsers = users => {
-//   // твой код
-//   return users.filter(user => !user.isActive);
-// };
+const getInactiveUsers = users => {
+  // твой код
+  return users.filter(user => !user.isActive);
+};
 
-// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-// const getUserWithEmail = (users, email) => {
-//   // твой код
-//   return users.find(user => user.email === email);
-// };
+const getUserWithEmail = (users, email) => {
+  // твой код
+  return users.find(user => user.email === email);
+};
 
-// console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-// console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
 
-// const getUsersWithAge = (users, min, max) => {
-//   // твой код
-//   return users.filter(user => user.age > min && user.age < max);
-// };
+const getUsersWithAge = (users, min, max) => {
+  // твой код
+  return users.filter(user => user.age > min && user.age < max);
+};
 
-// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
-// console.log(getUsersWithAge(users, 30, 40));
-// // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-// const calculateTotalBalance = users => {
-//   // твой код
-//   return users.reduce((balance, user) => balance + user.balance, 0);
-// };
+const calculateTotalBalance = users => {
+  // твой код
+  return users.reduce((balance, user) => balance + user.balance, 0);
+};
 
-// console.log(calculateTotalBalance(users)); // 20916
+console.log(calculateTotalBalance(users)); // 20916
 
 const getUsersWithFriend = (users, friendName) => {
   // твой код
@@ -67,29 +67,29 @@ const getUsersWithFriend = (users, friendName) => {
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// const getNamesSortedByFriendsCount = users => {
-//   // твой код
-//   return users
-//     .sort((userA, userB) => userA.friends.length - userB.friends.length)
-//     .map(user => user.name);
-// };
+const getNamesSortedByFriendsCount = users => {
+  // твой код
+  return users
+    .sort((userA, userB) => userA.friends.length - userB.friends.length)
+    .map(user => user.name);
+};
 
-// console.log(getNamesSortedByFriendsCount(users));
-// // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-// const getSortedUniqueSkills = users => {
-//   // твой код
-//   return users
-//     .reduce((allSkills, user) => {
-//       user.skills.forEach(skill => {
-//         if (!allSkills.includes(skill)) {
-//           allSkills.push(skill);
-//         }
-//       });
-//       return allSkills;
-//     }, [])
-//     .sort();
-// };
+const getSortedUniqueSkills = users => {
+  // твой код
+  return users
+    .reduce((allSkills, user) => {
+      user.skills.forEach(skill => {
+        if (!allSkills.includes(skill)) {
+          allSkills.push(skill);
+        }
+      });
+      return allSkills;
+    }, [])
+    .sort();
+};
 
-// console.log(getSortedUniqueSkills(users));
-// // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
